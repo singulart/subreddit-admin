54c35fc69efa451c1540793c89f2e253c44fefb6
singulart <serpian@gmail.com>
2024-06-03 10:05:48 -0400

    Fixing Swagger UI 401

--- src/main/java/net/lex/reddit/subs/config/SecurityConfiguration.java
+++ src/main/java/net/lex/reddit/subs/config/SecurityConfiguration.java
@@ -82,6 +82,7 @@ public class SecurityConfiguration {
                     .requestMatchers(mvc.pattern("/management/health/**")).permitAll()
                     .requestMatchers(mvc.pattern("/management/info")).permitAll()
                     .requestMatchers(mvc.pattern("/management/prometheus")).permitAll()
+                    .requestMatchers(mvc.pattern("/management/jhiopenapigroups")).permitAll()
                     .requestMatchers(mvc.pattern("/management/**")).hasAuthority(AuthoritiesConstants.ADMIN)
             )
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
